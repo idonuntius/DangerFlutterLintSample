56d774b237ad19c8813ae226270a56ce3e9eed29
idonuntius <mc.natsu831@gmail.com>
2021-11-23 00:54:13 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/idonuntius/flutterlintssample/flutter_lints_sample/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.idonuntius.flutterlintssample.flutter_lints_sample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TestWidget extends StatelessWidget {
+  TestWidget(this.name);
+
+  final String name;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container();
+  }
+}
